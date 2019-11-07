2df19403a2e006ee86e434520b3575ead70ea345
Jens Poenisch <poenisch@isym.tu-chemnitz.de>
2019-11-07 13:53:30 +0100

    added joining predicted lines according to kraken segment file

---
+++ kraken-img-split.py
@@ -0,0 +1,40 @@
+import argparse, json, os, sys
+from PIL import Image, ImageDraw
+
+'''
+Zerlegen eines PNG images mit Boxen von "kraken segmentation"
+'''
+
+def run():
+  parser = argparse.ArgumentParser(
+    description='Split image using kraken segmentation')
+  parser.add_argument('segments', metavar='seg',
+    help='segment file (json)')
+  parser.add_argument('image', metavar='img',
+    help='Image')
+  parser.add_argument('--dir', '-d', help='segment directory')
+  nargs = parser.parse_args()
+  args = vars(nargs)
+  print(args)
+  print(args['image'], args['segments'], args['dir'])
+
+  im = Image.open(args['image']).convert("RGBA")
+  draw = ImageDraw.Draw(im)
+  with open(args['segments'], 'r') as f:
+    data = json.load(f)
+  destDir = '.'
+  if args['dir'] is not None:
+    destDir = args['dir']
+    os.makedirs(destDir, exist_ok=True)
+  for i, b in enumerate(data['boxes']):
+    filePath = os.path.join(destDir, '{:03d}.png'.format(i))
+    x0, y0, x1, y1 = b
+    im1 = im.crop((x0, y0, x1, y1))
+    im1.save(filePath)
+  for i, b in enumerate(data['boxes']):
+    x0, y0, x1, y1 = b
+    draw.rectangle(((x0, y0), (x1, y1)), outline="red")
+  im.save(os.path.join(destDir, 'segments.png'))
+
+if __name__ == '__main__':
+  run()
---
+++ kraken-join-lines.py
@@ -0,0 +1,43 @@
+import argparse, json, os, sys
+from PIL import Image, ImageDraw
+
+'''
+Verbinden der Textzeilen mit kraken segment file
+'''
+
+def run():
+  parser = argparse.ArgumentParser(
+    description='Join predicted text using kraken segmentation')
+  parser.add_argument('segments', metavar='seg',
+    help='segment file (json)')
+  parser.add_argument('text', metavar='txt',
+    help='text file')
+  parser.add_argument('--dir', '-d', help='segment directory')
+  parser.add_argument('--pred', '-p', help='prediction file suffix',
+    default='pred.txt')
+  nargs = parser.parse_args()
+  args = vars(nargs)
+  print(args['text'], args['segments'], args['dir'])
+
+  with open(args['segments'], 'r') as f:
+    data = json.load(f)
+  destDir = '.'
+  if args['dir'] is not None:
+    destDir = args['dir']
+    os.makedirs(destDir, exist_ok=True)
+  lines = list()
+  for i, b in enumerate(data['boxes']):
+    filePath = os.path.join(destDir, '{:03d}.{:s}'.format(i, args['pred']))
+    x0, y0, x1, y1 = b
+    with open(filePath) as fi:
+      line = fi.read()
+      lines.append(line)
+    # TODO: Handle paragraphs
+    text = '\n'.join(lines)
+    with open(args['text'], 'w') as fo:
+      fo.write(text)
+      fo.write('\n')
+
+
+if __name__ == '__main__':
+  run()
